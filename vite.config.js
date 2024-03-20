f0acc629cdb579eef6c7c8f731684c6d9445f791
Luis Molina <alfonsomduque@gmail.com>
2024-03-19 21:08:35 -0600

    cambios

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://LuisMolinad.github.io/minimal-portafolio/",
   plugins: [react()],
 });
