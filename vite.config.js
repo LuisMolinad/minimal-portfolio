96605f2f1def7332f35289f3b0d999c7a0ba8edf
Luis Molina <alfonsomduque@gmail.com>
2024-03-19 20:38:45 -0600

    inicial retorno

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const base =
-  process.env.NODE_ENV === "production" ? "/minimal-portafolio/" : "/";
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: base, // Establece la ruta base
   plugins: [react()],
 });
